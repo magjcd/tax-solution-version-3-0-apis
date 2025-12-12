57692e1bd2bf3887de6dc4f9cf7a50a4189f3751
magjcd <magjcd@gmail.com>
2025-12-12 13:13:14 +0500

    spa configuration

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,15 @@
 <?php
 
-use App\Jobs\SignUpJob;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     // return view('welcome');
-//     // checkHelperFunc();
-//     // return \App\Models\User::has('roles')->get();
-//     // return \App\Models\User::with('roles')->get();
-//     $user = User::find(1);
-//     dispatch(new SignUpJob($user));
-// });
+// Route::get('/{any}', function () {
+//     return view('welcome'); // Or your SPA's main view
+// })->where('any', '.*');
 
-Route::get('/{any}', function () {
+Route::get('/', function () {
     return view('welcome'); // Or your SPA's main view
-})->where('any', '.*');
+});
+
+Route::get('{path}', function () {
+    return view('spa'); // Or your SPA's main view
+})->where('path', '(.*)');
